ebf2c56137879b21c68e4782842c4f4b95447e73
lbhtran <lbhtran@gmail.com>
2021-09-11 11:01:01 +0100

    Adapter and Bridge

---
+++ adapter/adapter_exercise_attempt.py
@@ -0,0 +1,26 @@
+class Square:
+    def __init__(self, side=0):
+        self.side = side
+
+
+def calculate_area(rc):
+    return rc.width * rc.height
+
+
+class SquareToRectangleAdapter:
+    def __init__(self, square):
+        self.square = square
+
+    @property
+    def width(self):
+        return self.square.side
+
+    @property
+    def height(self):
+        return self.square.side
+
+
+if __name__ == '__main__':
+    sqr = Square(9)
+    area = calculate_area(SquareToRectangleAdapter(sqr))
+    print(area)
